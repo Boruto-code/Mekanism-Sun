1475aeb8567b90c1643eec7d707353996173336b
Boruto-code <hamburger_nb_666@outlook.com>
2026-03-06 18:01:47 +0800

    Update

--- src/main/java/com/hamburger0abcde/mekanismsun/client/recipe_viewer/recipe/ArtificialSunRecipeViewerRecipe.java
+++ src/main/java/com/hamburger0abcde/mekanismsun/client/recipe_viewer/recipe/ArtificialSunRecipeViewerRecipe.java
@@ -1,10 +1,37 @@
 package com.hamburger0abcde.mekanismsun.client.recipe_viewer.recipe;
 
+import com.hamburger0abcde.mekanismsun.MekanismSun;
+import com.hamburger0abcde.mekanismsun.registries.MSChemicals;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import mekanism.api.SerializationConstants;
 import mekanism.api.chemical.ChemicalStack;
 import mekanism.api.recipes.ingredients.ChemicalStackIngredient;
+import mekanism.api.recipes.ingredients.creator.IngredientCreatorAccess;
+import mekanism.client.recipe_viewer.RecipeViewerUtils;
 import mekanism.client.recipe_viewer.emi.INamedRVRecipe;
+import mekanism.common.registries.MekanismChemicals;
 import net.minecraft.resources.ResourceLocation;
+
+import java.util.Collections;
+import java.util.List;
 
 public record ArtificialSunRecipeViewerRecipe(ResourceLocation id, ChemicalStackIngredient input, ChemicalStack output)
         implements INamedRVRecipe {
+
+    public static final Codec<ArtificialSunRecipeViewerRecipe> CODEC = RecordCodecBuilder.create(
+            instance -> instance.group(
+                    ResourceLocation.CODEC.fieldOf(SerializationConstants.ID).forGetter(ArtificialSunRecipeViewerRecipe::id),
+                    ChemicalStackIngredient.CODEC.fieldOf(SerializationConstants.INPUT).forGetter(ArtificialSunRecipeViewerRecipe::input),
+                    ChemicalStack.CODEC.fieldOf(SerializationConstants.OUTPUT).forGetter(ArtificialSunRecipeViewerRecipe::output)
+            ).apply(instance, ArtificialSunRecipeViewerRecipe::new)
+    );
+
+    public static List<ArtificialSunRecipeViewerRecipe> getArtificialSunRecipes() {
+        return Collections.singletonList(new ArtificialSunRecipeViewerRecipe(
+                RecipeViewerUtils.synthetic(MekanismSun.rl("helium"), "artificial_sun"),
+                IngredientCreatorAccess.chemicalStack().fromHolder(MekanismChemicals.HYDROGEN, 2),
+                MSChemicals.HELIUM.asStack(1)
+        ));
+    }
 }
--- src/main/java/com/hamburger0abcde/mekanismsun/client/recipe_viewer/type/MSRecipeViewerRecipeTypes.java
+++ src/main/java/com/hamburger0abcde/mekanismsun/client/recipe_viewer/type/MSRecipeViewerRecipeTypes.java
@@ -1,7 +1,14 @@
 package com.hamburger0abcde.mekanismsun.client.recipe_viewer.type;
 
+import com.hamburger0abcde.mekanismsun.MekanismSunLang;
+import com.hamburger0abcde.mekanismsun.client.recipe_viewer.recipe.ArtificialSunRecipeViewerRecipe;
+import com.hamburger0abcde.mekanismsun.registries.MSBlocks;
 import mekanism.client.recipe_viewer.type.FakeRVRecipeType;
 
 public class MSRecipeViewerRecipeTypes {
-
+    public static final FakeRVRecipeType<ArtificialSunRecipeViewerRecipe> ARTIFICIAL_SUN =
+            new FakeRVRecipeType<>(MSBlocks.ARTIFICIAL_SUN_CASING.getId(), MSBlocks.ARTIFICIAL_SUN_CASING,
+                    MekanismSunLang.ARTIFICIAL_SUN, ArtificialSunRecipeViewerRecipe.class,
+                    -3, -12, 168, 74, false,
+                    MSBlocks.ARTIFICIAL_SUN_CASING, MSBlocks.ARTIFICIAL_SUN_PORT);
 }
